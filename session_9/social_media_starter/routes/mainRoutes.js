ababf67447d8fc7057ea859c849179e3b4dc1be7
ArditDemolli98 <n_arditi_98@hotmail.com>
2024-12-05 07:10:21 +0100

    Social media completed

--- session_9/social_media_starter/routes/mainRoutes.js
+++ session_9/social_media_starter/routes/mainRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const data = require("../models/Main");
 
 router.get("/", (req, res)=>{
-    res.render("index", );
+    res.render("index", {data});
 });
 
 module.exports = router;
